98b31d5d7350295f9710037ff3cb33eff10f395b
jmswu <wu.zhuhua@gmail.com>
2018-06-14 10:57:16 +1200

    Update func.h

--- func.h
+++ func.h
@@ -16,11 +16,17 @@ extern "C" {
 #include <string.h>
 #include <stdlib.h>
 
-/* convert a string hex array to byte array, return data length */
+/* convert a string hex array to byte array, return data length 
+   @param - char *hex, string hex array, must be in one byte pair of "FF"
+   @param - uint8_t *data, return data of the converted data
+   return - length of the converted data
+*/
 uint8_t HexCharArray2ByteArray(char *hex, uint8_t *data);
   
 /* remove all space in a string 
-   
+   @param - char *input, input char array, terminated by \0
+   @param - char *output, output char array, terminated by \0
+   return - length of trimed char array
 */
 uint8_t trimSpace(char *input, char *output);
 
