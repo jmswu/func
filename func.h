e59152f484db4021fd151464c873aadfe60b34f9
jmswu <wu.zhuhua@gmail.com>
2018-06-14 10:51:20 +1200

    Update func.h

--- func.h
+++ func.h
@@ -18,6 +18,11 @@ extern "C" {
 
 /* convert a string hex array to byte array, return data length */
 uint8_t HexCharArray2ByteArray(char *hex, uint8_t *data);
+  
+/* remove all space in a string 
+   
+*/
+uint8_t trimSpace(char *input, char *output);
 
 
 #ifdef	__cplusplus
