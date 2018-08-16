5744d5dd1f2bcc301481e8e53f5bb25ff905fe9a
jmswu <wu.zhuhua@gmail.com>
2018-08-17 08:30:39 +1200

    Update func.h
    add parse()

--- func.h
+++ func.h
@@ -32,6 +32,29 @@ uint8_t trimSpace(char *input, char *output);
    
 // look for a sub string in a string, starting from offset location
 uint8_t isCmd(uint8_t *cmd, const unsigned char *cmd_to_check, uint8_t offset);
+   
+// parse a pattern in the buffer
+// if a pattern is match, return the pointer after the pattern
+// if a pattern is not match, return 0
+//
+//  Example #1
+//     char* cmd = "set param1 1024";
+//     puts(parse(parse(cmd, "set"), "param1"));
+//
+//  Example #2 
+//     char* val = 0;
+//     char* set = parse(cmd, "set");
+//     if(set){
+//         if(val = parse(set, "param1")){
+//             puts("set param1:");
+//             puts(val);
+//         }
+//         else if(val = parse(set, "param2")){
+//             puts("set param2:");
+//             puts(val);          
+//         }
+//     }
+uint8_t* parse(uint8_t* buffer, uint8_t* pattern);
 
 
 #ifdef	__cplusplus
