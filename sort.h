b119882e8dd2ad46d2b8c12223b9d14011f44617
jmswu <wu.zhuhua@gmail.com>
2019-06-06 21:06:47 +1200

    Update sort.h
    add comments

--- sort.h
+++ sort.h
@@ -1,13 +1,28 @@
 #ifndef SORT_H_INCLUDED
 #define SORT_H_INCLUDED
 
+#include <stdint-gcc.h>
+
+/*
+ * function call back for user compare implementation for all data types
+ */
 typedef int (*sort_compare_t)(void *arg1, void *arg2);
 
+/*
+ * function call back for user swap implementation for all data type
+ */
 typedef void (*sort_swap_t)(void *arg1, void *arg2);
 
-void sort_run(int*data, int n, sort_compare_t fwxCompare, sort_swap_t fxcSwap);
 
-
-
+/** \brief sort different data as user implementation
+ *
+ * \param int *data, pointer to data to be short, can be change to different data type
+ * \param int n, number of data
+ * \param sort_compare_t fxcCompare, user compare implementation
+ * \param sort_swap_t fxcSwap, user swap implementation
+ * \return void
+ *
+ */
+void sort_run(int *data, int n, sort_compare_t fxcCompare, sort_swap_t fxcSwap);
 
 #endif // SORT_H_INCLUDED
