4dd770128a3dea6c61cd6234be6b122aaf84c964
jmswu <wu.zhuhua@gmail.com>
2019-06-06 21:07:20 +1200

    Update sort.c
    fixed typo

--- sort.c
+++ sort.c
@@ -4,7 +4,7 @@
 /*
  * bubble sort
  */
-void sort_run(int*data, int n, sort_compare_t fxcCompare, sort_swap_t fxcSwap){
+void sort_run(int *data, int n, sort_compare_t fxcCompare, sort_swap_t fxcSwap){
     int isSwaped; // indicate data is swapped
 
     do {
@@ -12,11 +12,13 @@ void sort_run(int*data, int n, sort_compare_t fxcCompare, sort_swap_t fxcSwap){
 
         // loop through all data
         for(int i = 0; i < n - 1; i++){
+
             if (fxcCompare(&data[i], &data[i + 1])){ // compare data
-                fxcSwap(&data[i], &data[i + 1]);
+                fxcSwap(&data[i], &data[i + 1]); // swap data
                 isSwaped = 1; //indicate data is swapped
             }
         }
 
     }while(isSwaped == 1); // repeat if data is swapped
 }
+
