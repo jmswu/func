61f3e9500f88784e118e7a10c5d588f22596c406
jmswu <wu.zhuhua@gmail.com>
2019-06-09 19:22:21 +1200

    Update sort.c
    update sort algorithm

--- sort.c
+++ sort.c
@@ -6,18 +6,21 @@
  */
 void sort_run(int *data, int n, sort_compare_t fxcCompare, sort_swap_t fxcSwap){
     int isSwaped; // indicate data is swapped
+    int pass = 0;
 
     do {
         isSwaped = 0; // reset flag
 
         // loop through all data
-        for(int i = 0; i < n - 1; i++){
+        for(int i = 0; i < n - pass - 1; i++){
 
             if (fxcCompare(&data[i], &data[i + 1])){ // compare data
                 fxcSwap(&data[i], &data[i + 1]); // swap data
                 isSwaped = 1; //indicate data is swapped
             }
         }
+        
+        pass++;
 
     }while(isSwaped == 1); // repeat if data is swapped
 }
